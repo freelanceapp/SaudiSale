e37529a1a2b29522c7972b27dfea73b3aaad36f5
Emad <emadmagdy.developer@gmail.com>
2021-01-21 17:02:02 +0200

    emad

---
+++ app/src/main/java/com/saudi_sale/activities_fragments/activite_swear/SwearActivity.java
@@ -0,0 +1,16 @@
+package com.saudi_sale.activities_fragments.activite_swear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saudi_sale.R;
+
+public class SwearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swear);
+    }
+}
